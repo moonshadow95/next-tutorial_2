137ef9ccaf93999bbf86c63dd9174ee924390563
Moonshadow <last_enemy@naver.com>
2023-04-19 23:28:44 +0900

    Next component

---
+++ pages/cart/data.tsx
@@ -0,0 +1,3 @@
+let age = 20;
+let name = 'zzong'
+export {age, name};
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,21 +1,25 @@
+'use client'
 import React from 'react';
+import {age, name} from "./data";
+
 
 const Cart = () => {
   return (
     <div>
       <h4 className={'title'}>Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem/>
     </div>
   )
 };
 
 export default Cart;
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명 {age}</p>
+      <p>$40 {name}</p>
+      <p>1개</p>
+    </div>
+  )
+}
